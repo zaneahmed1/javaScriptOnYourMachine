f247117fa599de5bf59fe2281199dc4eaace4a71
Zane <zaneahmed4@gmail.com>
2022-01-24 20:50:19 -0500

    updated

--- index.js
+++ index.js
@@ -1,8 +1,24 @@
-function addFruit(fruits, fruit) {
+// function addFruit(fruits, fruit) {
+//     fruits.push(fruit);
+//    // console.log(fruits);
+//     return fruits;
+//   }
+  
+//   const fruits = ["🍓", "🍐", "🍎"];
+//   addFruit(fruits, "🥝");
+//  // process.stdout.write("this should log")
+//  console.log(process.argv);
+ function addFruit(fruits, fruit) {
     fruits.push(fruit);
-    
     return fruits;
   }
   
   const fruits = ["🍓", "🍐", "🍎"];
-  addFruit(fruits, "🥝");
+  console.log(process.argv);
+  const input = process.argv[2];
+  if (input) {
+    let result = addFruit(fruits, input);
+    console.log(result);
+  } else {
+    console.log("No fruit added...");
+  }
